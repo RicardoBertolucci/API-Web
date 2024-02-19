104e68a9831eb3587cf5412a62d37b2cd6aeead0
RicardoBertolucci <Ric.bertolucci@gmail.com>
2024-02-19 00:29:15 -0300

    CREATE: layout of schedule

--- API-Frontend/src/App.js
+++ API-Frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './assets/components/Header/Header';
+import Notebook from './assets/components/Notebook/Notebook';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Notebook />
     </div>
   );
 }
--- API-Frontend/src/assets/components/Header/Header.js
+++ API-Frontend/src/assets/components/Header/Header.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import style from './Header.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 
 const Header = () => {
   return (
-    <div>
-      Ricardo
+    <div className={style.containerSchedule}>
+      <h1 className={style.titleSchedule}>Agenda Telefônica</h1>
+      <div className={style.btnSchedule}>
+        <div>
+          <input type="text"  className={style.inputSearch}/>
+          <button className={style.pesquisar}><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+        </div>
+        <div>
+          <button className={style.cadastrar}>Cadastrar</button>
+        </div>
+      </div>
     </div>
   )
 }
